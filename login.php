ff91813b87bdc8b493c293113fe453de4135764f
Samantha Castro <126586370+Samycastro03@users.noreply.github.com>
2023-04-25 12:30:15 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lista</title>
+</head>
+<body bgcolor="#D8BFD8"> <center>
+Login:<br></br>
+<div class="form-floating mb-3">
+<form class="form-floating">
+<label for="floatingInputInvalid">Endereço de email</label><br>
+  <input type="email" class="form-control is-invalid" id="floatingInputInvalid" placeholder="name@example.com">
+ </form><br>
+  <form class="row g-3">
+  <div class="col-auto">
+    <label for="inputPassword2" class="visually-hidden">Senha</label><br>
+    <input type="senha" class="form-control" id="inputPassword2">
+  </div><br>
+  <div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Confirmar</button></p>
+</center>
+  </div>
+</form>
+ </form>
+</form>
+</div>
+</body>
+</html>
